e93f112085daa350ef3eb0331c79267eaa7ea931
Matt Zimmer <zimmermja@gmail.com>
2021-12-14 20:29:22 +0000

    Initiated rspec converage checker with simplecov

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      p "NOTE: build a page for going to 'Profile' when not logged in"
       # get "/users/index"
       # expect(response).to have_http_status(:success)
     end
