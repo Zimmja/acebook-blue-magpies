78d8f2a9968fbedaa1474076ad9dff28d996efe6
Matt Zimmer <zimmermja@gmail.com>
2021-12-15 08:26:47 +0000

    removed application controller spec; may need to re-add following future merge

--- spec/controllers/application_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationController, type: :controller do
-  # describe "Redirect users" do
-  #   it "Redirects to the login_path when not logged in" do
-  #     # expect(subject.require_user).to eq nil
-  #   end
-  # end
-end
