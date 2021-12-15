4f8f952f1343fff37f7dd9f61589a05b2c0e848c
Matt Zimmer <zimmermja@gmail.com>
2021-12-15 11:39:50 +0000

    Prevented deletion of posts by other users

--- spec/features/posts_can_be_liked_spec.rb
+++ spec/features/posts_can_be_liked_spec.rb
@@ -12,6 +12,7 @@ RSpec.feature "Posts can be liked", type: :feature do
   end
 
   scenario "Amount of likes for a post are displayed" do  # I have not found correct syntax to select from drop down menu to correct error
+    puts "Fix text in posts_can_be_liked_spec.rb"
     select "Like", :from => "like[like_option]"
     expect(page).to have_content '1'
   end
