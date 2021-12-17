d41b422370fec9e51349d012d6f63acb47603ca0
Matt Zimmer <zimmermja@gmail.com>
2021-12-17 14:08:26 +0000

    fixed bug preventing posts with comments from being deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
 
   belongs_to :user
-  has_many_attached :images
-  has_many :comments
+  has_many_attached :images, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy #dependent: :destroy will delete likes if post/user is deleted
   validates :message, presence: true
   validate :image_type
